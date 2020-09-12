3c474aa92a9ef088ba62c92ff3ba16b087cb6fac
Sebastian Curtis T. Lavarias <denzilbao@gmail.com>
2020-09-12 11:19:52 +0800

    semi finished HighestRatedGuidesSection.vue

--- web-client/src/components/home/HighestRatedGuidesSection.vue
+++ web-client/src/components/home/HighestRatedGuidesSection.vue
@@ -8,27 +8,46 @@
         <h1 class="display-1 mb-1">Highest Rated Guides</h1>
         <div class="custom-divider"></div>
       </div>
-      <carousel :items="4" :dots="false" :nav="false" :autoplay="false">
+      <v-slide-group v-model="profilePreview" show-arrows>
         <template v-for="n in 12">
-          <guide-profile-preview :key="n" class-name="ma-3">
-          </guide-profile-preview>
+          <v-slide-item :key="n" v-slot:default="{ active, toggle }">
+            <span @click="toggle">
+              <guide-profile-preview
+                :class-name="`${n === 12 ? '' : 'mr-5'} mb-10`"
+              >
+              </guide-profile-preview>
+            </span>
+          </v-slide-item>
         </template>
-        <template slot="prev">
-          <v-btn> </v-btn>
+        <template v-slot:prev>
+          <v-btn fab small color="secondary">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
         </template>
-        <template slot="next"><span class="next">next</span></template>
-      </carousel>
+        <template v-slot:next>
+          <v-btn fab small color="secondary">
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
+        </template>
+      </v-slide-group>
     </v-container>
   </section>
 </template>
 
 <script>
-import Carousel from "vue-owl-carousel";
 import GuideProfilePreview from "@/components/GuideProfilePreview";
 
 export default {
   name: "home-highest-rated-guides-section",
 
-  components: { GuideProfilePreview, Carousel },
+  components: { GuideProfilePreview },
+
+  data() {
+    return {
+      profilePreview: null,
+      multiple: false,
+      showArrows: true,
+    };
+  },
 };
 </script>
