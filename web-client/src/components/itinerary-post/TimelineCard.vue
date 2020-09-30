a5ef5ad4edbdfefe26a5cbdb71072890cd6005eb
Sebastian <denzilbao@gmail.com>
2020-09-30 09:23:50 +0800

    [PF-15] Itinerary Post Details Page Design
    Save progress.

--- web-client/src/components/itinerary-post/TimelineCard.vue
+++ web-client/src/components/itinerary-post/TimelineCard.vue
@@ -1,44 +1,66 @@
 <template>
   <v-card outlined tile>
-    <v-card-title>Itinerary</v-card-title>
-    <v-expansion-panels accordion flat :value="0">
-      <template v-for="n in 5">
-        <v-expansion-panel :key="n">
-          <v-expansion-panel-header>
-            <div class="d-flex justify-space-between align-center">
-              <span class="body-2">
-                <span class="font-weight-bold">Day {{ n }}</span>
-                <span class="mx-1">-</span>
-                <span class="font-italic">Lorem ipsum dolor sit amet.</span>
-              </span>
-              <span class="body-2"> May 1{{ n }}, 2000 </span>
-            </div>
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <v-data-table
-              :headers="dataTableHeaders"
-              :items="itineraryTimeline"
-              item-key="name"
-              show-expand
-              hide-default-footer
-            >
-              <template v-slot:item.time="{ item }">
-                <span>
-                  <v-icon small class="mr-1">mdi-clock-outline</v-icon>
-                  <span>{{ item.time }}</span>
-                </span>
-              </template>
-              <template v-slot:item.totalExpenses="{ item }">
-                <span>&#8369; {{ item.totalExpenses }}</span>
-              </template>
-              <template v-slot:expanded-item="{ headers, item }">
-                <td :colspan="headers.length"></td>
-              </template>
-            </v-data-table>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
+    <v-card-title class="font-weight-bold">Itinerary</v-card-title>
+    <v-data-table
+      :headers="itineraryTimelineDataTableHeaders"
+      :items="itineraryTimeline"
+      hide-default-footer
+    >
+      <template v-slot:item.day="{ item }">
+        <span class="font-weight-bold">Day {{ item.day }}</span>
       </template>
-    </v-expansion-panels>
+      <template v-slot:item.numberOfDestinations="{ item }">
+        <span class="text-capitalize">{{ item.numberOfDestinations }}</span>
+      </template>
+      <template v-slot:item.totalExpenses="{ item }">
+        <span>&#8369; {{ item.totalExpenses }}</span>
+      </template>
+      <template v-slot:item.action="{ item }">
+        <v-btn icon @click="isItineraryDetailsDialogOpen = true">
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </template>
+    </v-data-table>
+    <v-dialog v-model="isItineraryDetailsDialogOpen" width="800">
+      <v-card>
+        <v-card-title>
+          <span class="font-weight-bold">Day 1</span>
+          <div class="flex-grow-1"></div>
+          <v-btn icon @click="isItineraryDetailsDialogOpen = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-subtitle>May 21, 2020</v-card-subtitle>
+        <v-card-text>
+          <v-data-table
+            :headers="itineraryTimelineTimestampTableHeaders"
+            :items="itineraryTimelineTimestamps"
+            hide-default-footer
+            :single-expand="singleExpand"
+            item-key="time"
+            show-expand
+          >
+            <template v-slot:item.time="{ item }">
+              <v-icon class="mr-1" small>mdi-clock-outline</v-icon>
+              <span>{{ item.time }}</span>
+            </template>
+            <template v-slot:item.expenses="{ item }">
+              <span>&#8369; {{ item.expenses }}</span>
+            </template>
+            <template v-slot:expanded-item="{ headers, item }">
+              <td :colspan="headers.length">
+                <div class="py-3">
+                  <h2 class="subtitle-2 d-block mb-1">Other Details:</h2>
+                  <span class="body-2 text-capitalize">
+                    {{ item.otherDetails }}
+                  </span>
+                </div>
+              </td>
+            </template>
+          </v-data-table>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -48,16 +70,16 @@ export default {
 
   data() {
     return {
-      dataTableHeaders: [
+      itineraryTimelineDataTableHeaders: [
         {
-          text: "Time",
-          align: "start",
-          value: "time",
+          text: "Day",
+          value: "day",
           sortable: true,
+          align: "left",
         },
         {
-          text: "Destination",
-          value: "destination",
+          text: "# of Destinations",
+          value: "numberOfDestinations",
           sortable: false,
         },
         {
@@ -65,34 +87,74 @@ export default {
           value: "totalExpenses",
           sortable: true,
         },
-        { text: "", value: "data-table-expand" },
+        {
+          text: "",
+          value: "action",
+          align: "right",
+          sortable: false,
+        },
       ],
       itineraryTimeline: [
         {
+          day: 1,
+          numberOfDestinations: "3 Destinations",
+          totalExpenses: 5000,
+        },
+        {
+          day: 2,
+          numberOfDestinations: "3 Destinations",
+          totalExpenses: 5000,
+        },
+        {
+          day: 3,
+          numberOfDestinations: "3 Destinations",
+          totalExpenses: 5000,
+        },
+      ],
+      isItineraryDetailsDialogOpen: false,
+      itineraryTimelineTimestampTableHeaders: [
+        {
+          text: "Time",
+          value: "time",
+          sortable: true,
+          align: "left",
+        },
+        {
+          text: "Destination",
+          value: "destination",
+          sortable: false,
+        },
+        {
+          text: "Expenses",
+          value: "expenses",
+          sortable: true,
+        },
+        { text: "", value: "data-table-expand" },
+      ],
+      itineraryTimelineTimestamps: [
+        {
           time: "10:00AM",
-          destination: "Destination Name",
-          totalExpenses: 40.0,
+          destination: "Destination 1",
+          expenses: 500,
+          otherDetails:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id nisi et enim dictum dignissim. Proin volutpat risus id efficitur ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Vestibulum molestie ligula at elit malesuada fermentum. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas a eros a arcu condimentum sollicitudin vitae ut mauris. Ut ut odio non diam varius pharetra. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+        {
+          time: "1:00PM",
+          destination: "Destination 2",
+          expenses: 500,
+          otherDetails:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id nisi et enim dictum dignissim. Proin volutpat risus id efficitur ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Vestibulum molestie ligula at elit malesuada fermentum. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas a eros a arcu condimentum sollicitudin vitae ut mauris. Ut ut odio non diam varius pharetra. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+        {
+          time: "5:00PM",
+          destination: "Destination 3",
+          expenses: 500,
+          otherDetails:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id nisi et enim dictum dignissim. Proin volutpat risus id efficitur ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Vestibulum molestie ligula at elit malesuada fermentum. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas a eros a arcu condimentum sollicitudin vitae ut mauris. Ut ut odio non diam varius pharetra. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         },
       ],
-      tab: null,
-      tabs: [
-        {
-          text: "Restaurant",
-          icon: "mdi-silverware",
-        },
-        {
-          text: "Lodging",
-          icon: "mdi-bed",
-        },
-        {
-          text: "Transportation",
-          icon: "mdi-train-car",
-        },
-        {
-          text: "Activity",
-          icon: "mdi-hiking",
-        },
-      ],
+      singleExpand: true,
     };
   },
 };
