e62e9e992496c654b90210ddb2e2fc54b8b42f6d
Sebastian <denzilbao@gmail.com>
2020-10-03 17:52:52 +0800

    [PF-23] Tour Guides Page Design
    Initial design created.

--- web-client/src/pages/TourGuides.vue
+++ web-client/src/pages/TourGuides.vue
@@ -1,23 +1,45 @@
 <template>
   <section>
-    <v-container>
-      <v-row>
-        <v-col cols="12">
-          <v-row>
-            <v-col cols="12">
-              <tour-guides-highest-rated-section></tour-guides-highest-rated-section>
-            </v-col>
-            <v-col cols="12">
-              <v-divider></v-divider>
-            </v-col>
-            <v-col cols="12">
-              <tour-guides-discover-more-section></tour-guides-discover-more-section>
-            </v-col>
-            <v-col cols="12"> </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
+    <tour-guides-header></tour-guides-header>
+    <div id="tour-guides-content">
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-toolbar>
+              <v-autocomplete
+                outlined
+                single-line
+                label="Search Destination"
+                dense
+                hide-details
+                append-icon="mdi-magnify"
+              ></v-autocomplete>
+              <v-spacer></v-spacer>
+              <custom-tooltip-button
+                icon="mdi-filter"
+                text="Filter Tour Guides"
+              ></custom-tooltip-button>
+              <custom-tooltip-button
+                icon="mdi-sort"
+                text="Sort Tour Guides"
+              ></custom-tooltip-button>
+            </v-toolbar>
+          </v-col>
+          <v-col>
+            <v-row dense>
+              <v-col cols="12">
+                <span class="subtitle-1">Results</span>
+              </v-col>
+              <template v-for="n in 9">
+                <v-col cols="12" md="4" :key="n">
+                  <generic-tour-guide-profile-preview></generic-tour-guide-profile-preview>
+                </v-col>
+              </template>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
     <generic-about-local-tour-guides-section></generic-about-local-tour-guides-section>
     <generic-footer></generic-footer>
   </section>
@@ -25,14 +47,16 @@
 
 <script>
 import GenericFooter from "@/components/generic/Footer";
-import TourGuidesHighestRatedSection from "@/components/tour-guides/sections/HighestRated";
-import TourGuidesDiscoverMoreSection from "@/components/tour-guides/sections/DiscoverMore";
 import GenericAboutLocalTourGuidesSection from "@/components/generic/section/AboutLocalTourGuides";
+import TourGuidesHeader from "@/components/tour-guides/Header";
+import CustomTooltipButton from "@/components/custom/TooltipButton";
+import GenericTourGuideProfilePreview from "@/components/generic/TourGuideProfilePreview";
 export default {
   components: {
+    GenericTourGuideProfilePreview,
+    CustomTooltipButton,
+    TourGuidesHeader,
     GenericAboutLocalTourGuidesSection,
-    TourGuidesDiscoverMoreSection,
-    TourGuidesHighestRatedSection,
     GenericFooter,
   },
 };
