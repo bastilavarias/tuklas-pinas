477e030f001fa36dab76db645fbff2831f28ac72
Sebastian <denzilbao@gmail.com>
2020-10-04 12:48:23 +0800

    [PF-31] Notification page design.
    Initial design created.

--- web-client/src/pages/Notification.vue
+++ web-client/src/pages/Notification.vue
@@ -3,7 +3,29 @@
     <v-container>
       <v-row>
         <v-col cols="12" md="9">
-          <v-row></v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-card outlined>
+                <v-toolbar flat class="mb-10">
+                  <v-toolbar-title class="title font-weight-bold"
+                    >Notifications</v-toolbar-title
+                  >
+                  <v-spacer></v-spacer>
+                  <v-chip>
+                    <v-icon left> mdi-tune </v-icon>
+                    <span>Filter Notifications</span>
+                  </v-chip>
+                </v-toolbar>
+                <template v-for="n in 10">
+                  <generic-notification-list-item
+                    :key="n"
+                    :is-read="n % 2 !== 0"
+                  ></generic-notification-list-item>
+                  <v-divider v-if="n !== 10"></v-divider>
+                </template>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-col>
         <v-col cols="12" md="3">
           <v-row>
@@ -22,7 +44,12 @@
 <script>
 import GenericEventsExplorerPreviewSideCard from "@/components/generic/EventsExplorerPreviewSideCard";
 import GenericStickyFooterCard from "@/components/generic/StickyFooterCard";
+import GenericNotificationListItem from "@/components/generic/notification/ListItem";
 export default {
-  components: { GenericStickyFooterCard, GenericEventsExplorerPreviewSideCard },
+  components: {
+    GenericNotificationListItem,
+    GenericStickyFooterCard,
+    GenericEventsExplorerPreviewSideCard,
+  },
 };
 </script>
