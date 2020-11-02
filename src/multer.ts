321f47e73fb463f46fd6bd53824d3e24f4c89388
Sebastian <denzilbao@gmail.com>
2020-11-02 11:11:56 +0800

    added auto creation of folder in multer.

--- src/multer.ts
+++ src/multer.ts
@@ -1,9 +1,12 @@
 import Multer, { diskStorage } from "multer";
+const fs = require("fs-extra");
 
 const multer = Multer({
   storage: diskStorage({
     destination(_request, _file, callback) {
-      callback(null, process.cwd() + "/tmp/files");
+      const directory = process.cwd() + "/tmp/files";
+      fs.ensureDirSync(directory);
+      callback(null, directory);
     },
   }),
 });
